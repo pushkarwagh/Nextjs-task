4cd991e7abf9e0d26c731ae4c247a343991b8bbc
pushkarwagh <puwagh@bestpeers.com>
2022-05-24 15:44:00 +0530

    persistor

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,24 @@
-import { Provider } from 'react-redux'
-import store from '../redux/store'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+import store from '../redux/store';
+
 import '../styles/globals.css'
+
+let persistor = persistStore(store);
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   )
 }
 
 export default MyApp
+
+
+
+
--- redux/store.js
+++ redux/store.js
@@ -1,9 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
+
 import albumSlice from "./reducers/slice";
 
-const store = configureStore({
-  reducer: {
-    album: albumSlice,
-  },
+const reducers = combineReducers({
+  album: albumSlice,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
+});
+
 export default store;
